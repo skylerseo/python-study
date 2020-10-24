4c35a23488267feb4714bc4d11744c4edbab7c74
Skyler <skylerseo.dev@gmail.com>
2020-10-24 23:20:44 +0900

    Update 003.character-and-data-types.py

---
+++ 003.character-and-data-types.py
@@ -0,0 +1,35 @@
+# 문자의 표현
+print('Hello')
+print("Hello")
+print("Hello 'world'")
+print('Hello "world"')
+print('---------------------')
+
+# 문자열의 제어
+print('Hello '+'world')
+print('Hello '*3)
+print('Hello'[0])
+print('Hello'[1])
+print('Hello'[2])
+print('---------------------')
+
+# 문자열의 제어2
+print('HELLO WORLD'.capitalize())
+print('hello world'.upper())
+print('hello world'.__len__())
+print(len('hello world'))
+print('Hello world'.replace('world', 'programming'))
+print('---------------------')
+
+# 특수한 문자들
+print("Skyler's \"Github\"")
+print("\\")
+print("Hello\nworld")
+print("Hello\t\tworld")
+print("\a")
+print('Hello\nworld')
+print('---------------------')
+
+# 문자와 숫자를 통해서 알아보는 데이터 타입
+print(1+2)
+print("1"+"2")
