b0690644617332f4452bfabcf1b3418b50ab5355
Skyler <skylerseo.dev@gmail.com>
2020-10-24 22:28:05 +0900

    Update 002.arithmetical-operations.py

---
+++ 002.arithmetical-operations.py
@@ -0,0 +1,10 @@
+print(10 + 5)
+print(10 - 5)
+print(10 * 5)
+print(10 / 5)
+
+import math
+print(math.ceil(2.2)) # 올림
+print(math.floor(2.7)) # 내림
+print(math.pow(2,10)) # 2의 10승. (제곱을 표현할 때 사용)
+print(math.pi) # 파이. 원주율
