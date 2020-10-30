410e34c3f1739e22ba9c5ce2815bc2dd749cddb5
Skyler <skylerseo.dev@gmail.com>
2020-10-30 21:05:05 +0900

    011.container.py

---
+++ 011.container.py
@@ -0,0 +1,10 @@
+print(type('bogi')) #<class 'str'>
+name = 'bogi'
+print(name) #bogi
+print(type(['bogi', 'leezche', 'graphittie'])) #<class 'list'>
+names = ['bogi', 'leezche', 'graphittie']
+print(names) 
+print(names[2]) #graphittie
+bogi = ['dog', 'seoul', 11, False]
+bogi[1] = 'busan'
+print(bogi) #['dog', 'busan', 11, False]
